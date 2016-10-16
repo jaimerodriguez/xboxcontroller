b34a8c396ec079cb79e0f2f6bd07cde3966cbdd5
Your Name <you@example.com>
2016-10-16 11:11:10 -0700

    for my reference mostly

---
+++ Assets/Scripts/ControllerListener.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerListener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.ClearDeveloperConsole(); 
+         
+        KeyCode[] keys = new KeyCode[]
+            {
+                KeyCode.Joystick1Button0, KeyCode.Joystick1Button1, KeyCode.Joystick1Button2,
+                KeyCode.Joystick1Button3, KeyCode.Joystick1Button4, KeyCode.Joystick1Button5,
+                KeyCode.Joystick1Button6, KeyCode.Joystick1Button7, KeyCode.Joystick1Button8,
+                KeyCode.Joystick1Button9,
+                KeyCode.Joystick1Button10, KeyCode.Joystick1Button11, KeyCode.Joystick1Button12,
+                KeyCode.Joystick1Button13, KeyCode.Joystick1Button14, KeyCode.Joystick1Button15,
+                KeyCode.Joystick1Button16, KeyCode.Joystick1Button17, KeyCode.Joystick1Button18,
+                KeyCode.Joystick1Button19,
+            }; 
+       
+        foreach (KeyCode key in keys )
+        {
+            if (Input.GetKeyDown(key))
+                Debug.Log("Key:"  + key.ToString()); 
+        }
+
+        string[] axii = new string[]
+        {
+            "DPad_XAxis_1",
+            "DPad_YAxis_1",
+            "Triggers_1",
+            "R_YAxis_1" ,
+            "R_XAxis_1" ,
+            "L_YAxis_1" ,
+            "L_XAxis_1" ,
+        }; 
+
+
+        foreach ( string axis in axii )
+        {
+            float f = Input.GetAxisRaw(axis);
+            if ( Mathf.Abs(f) > 0.1 )
+            Debug.Log( "Trigger" + axis + ":" + f.ToString() ); 
+        }
+
+        string[] buttons = new string[]
+        {
+            "Start",
+            "A_1",
+            //"B",
+            //"X"
+        }; 
+
+        foreach ( string button in buttons )
+        {
+            if ( Input.GetButton(button))
+            {
+                Debug.Log("Button:" + button); 
+            }
+        }
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# xboxcontroller
+# Xbox Controller
+
+This is just a reference InputManager.asset file for Xbox controller on Unity.
+
+I find it useful for reminding me value ranges for axis and mappings of the buttons (to KeyCode's in Unity). 
+  
+![](./ScreenCap.png)
+
+
+### Usage Tips:
+- To add the InputManager.asset into a project, copy it to the *ProjectSettings* folder in your project. 
+- This only has input for xbox, so most likely you will still need to add standard axis for other input modules. 
+
+
